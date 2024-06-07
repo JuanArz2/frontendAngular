df262bd0b2e19469c99169bd04b0d2009e816a48
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-06-07 17:32:51 -0500

    work

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { FurnitureComponent } from "./components/furniture/furniture.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", title: "Home", component: HomeComponent},
+    {path: "login", title: "Iniciar Sesión", component: LoginComponent},
+    {path: "shop", title: "Mobiliario", component: FurnitureComponent},
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "**", title: "404 | Página no Encontrada", component: PageNotFoundComponent},
+];
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from "@angular/forms";
+import { LoginCredentials } from "../../interfaces/loginCredentials";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginCredentialsData = new FormGroup({
+    email: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required),
+  });
 
+  handleSubmit() {
+    if (this.loginCredentialsData.valid) {
+      const email = this.loginCredentialsData.value.email;
+      const password = this.loginCredentialsData.value.password;
+
+      if (typeof email === "string" && typeof password === "string") {
+        const credentialModel: LoginCredentials = {
+          email,
+          password,
+        };
+        console.log("crendential ", credentialModel)
+      }
+    } else {
+      console.log("Empty fields");
+    }
+  }
 }
