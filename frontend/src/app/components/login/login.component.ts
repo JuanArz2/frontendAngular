cf5338f57f61b0a901c0f88672cf66ecff6fbef0
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-06-07 22:19:04 -0500

    token, decodified token, greats user on Shop after login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { JwtHelperService } from "@auth0/angular-jwt";
 import { LoginCredentials } from "../../interfaces/loginCredentials";
 import { LoginService } from "../../services/login.service";
+
+const jwtHelperService = new JwtHelperService();
 
 @Component({
   selector: 'app-login',
@@ -11,6 +15,7 @@ import { LoginService } from "../../services/login.service";
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  router = inject(Router);
   loginService: LoginService = inject(LoginService);
 
   loginCredentialsData = new FormGroup({
@@ -28,9 +33,13 @@ export class LoginComponent {
           email,
           password,
         };
-        this.loginService.login(credentials).subscribe((res: any)=>{
-          console.log("Response: ", res)
-        }) // any porque si no hacemos el Backend no sabemos qué vendrá de él
+        this.loginService.login(credentials).subscribe((res: any) => { // any porque si no hacemos el Backend no sabemos qué vendrá de él
+          //console.log("Response: ", res);
+          const decoded = jwtHelperService.decodeToken(res.data.token);
+          //console.log("decoded: ", decoded);
+          localStorage.setItem("token", res.data.token);
+          this.router.navigateByUrl("/shop");
+        });
       }
     } else {
       console.log("Empty form");
--- frontend/src/app/components/shop/shop.component.ts
+++ frontend/src/app/components/shop/shop.component.ts
@@ -1,4 +1,8 @@
+import { Token } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { JwtHelperService } from "@auth0/angular-jwt";
+
+const jwtHelperService = new JwtHelperService();
 
 @Component({
   selector: 'app-shop',
@@ -8,5 +12,12 @@ import { Component } from '@angular/core';
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
-
+  userName: String = "";
+  ngOnInit(){
+    const token: any = localStorage.getItem("token");
+    //console.log("Token: ", token);
+    const decoded = jwtHelperService.decodeToken(token);
+    //console.log("Decoded: ", decoded.name);
+    this.userName = decoded.name;
+  };
 }
