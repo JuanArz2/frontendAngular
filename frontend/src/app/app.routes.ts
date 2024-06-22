5ea3e2e7d704286f8bd124f09d747f47abc818b0
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-06-21 19:30:53 -0500

    Components created: shop & about-us, routered and set in Nav

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,17 @@
-import { Routes } from '@angular/router';
-import { activateGuard } from "./guards/activate.guard";
+import { Routes } from '@angular/router'; // Para poder crear rutas
+import { activateGuard } from "./guards/activate.guard"; // Para denegar acceso sin credenciales
+import { AboutUsComponent } from "./components/about-us/about-us.component";
 import { HomeComponent } from "./components/home/home.component";
 import { LoginComponent } from "./components/login/login.component";
+import { ProductsComponent } from "./components/products/products.component";
 import { ShopComponent } from "./components/shop/shop.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
+    {path: "aboutUs", title: "Willi Bastiadas", component: AboutUsComponent},
     {path: "home", title: "Inicio", component: HomeComponent},
     {path: "login", title: "Iniciar Sesión", component: LoginComponent},
+    {path: "products", title: "Creaciones", component: ProductsComponent},
     {path: "shop", title: "Tienda", component: ShopComponent, canActivate:[activateGuard]},
     {path: "", redirectTo: "home", pathMatch: "full"},
     {path: "**", title: "404 | Página no Encontrada", component: PageNotFoundComponent},
