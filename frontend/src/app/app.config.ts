6ffcc32fe902fe608aa9dc76c113da32e2e7198f
Juan Ariza <160557685+JuanArz2@users.noreply.github.com>
2024-08-08 18:24:34 -0500

    Ngrx instalado e importado en app.config.ts > providers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,6 +3,9 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from "@angular/common/http";
 import { provideAnimations } from "@angular/platform-browser/animations"; // para npm mgx-toastr (librería prompts)
 import { provideToastr } from "ngx-toastr";
+import { provideStore } from "@ngrx/store";
+import { provideEffects } from "@ngrx/effects";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
 
 import { routes } from './app.routes';
 
@@ -10,5 +13,9 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), 
     provideHttpClient(), 
     provideAnimations(), 
-    provideToastr()]
+    provideToastr(),
+    provideStore({}), 
+    provideEffects(), 
+    provideStoreDevtools({})
+  ]
 };
